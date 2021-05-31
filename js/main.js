403692aa8b641726befa9fe7a0e03d4e6d6aacad
HonduranCoder <stef45689@gmail.com>
2021-05-31 15:59:18 -0500

    More

--- js/main.js
+++ js/main.js
@@ -303,4 +303,5 @@ return Math.pow( sqRt + 1, 2 )
 
 //Jaden Casing Strings
 String.prototype.toJadenCase = function () {
-return this.split(' ')
+  return this.split(' ')
+  map(w => w[0].toUpperCase() + w.slice(1)).join(' ');
